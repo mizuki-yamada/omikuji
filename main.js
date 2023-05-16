6da3d8c45c3fbd22d502b0f1fcdd2e39d359ba03
mizuki-yamada <my79909369@gmail.com>
2023-05-16 21:41:26 +0900

    条件分岐を配列で書き換えた

--- main.js
+++ main.js
@@ -5,18 +5,9 @@
     const result = document.getElementById('result');
 
     btn.addEventListener('click', () => {
-        const n = Math.floor(Math.random() * 3);
-        switch (n) {
-            case 0:
-                result.textContent = '大吉';
-                break;
-            case 1:
-                result.textContent = '中吉';
-                break;
-            case 2:
-                result.textContent = '凶';
-                break;
-        }
- 
+        const results = ['大吉', '中吉', '凶','小吉'];
+        const n = Math.floor(Math.random() * results.length);
+
+        result.textContent = results[n];
     })
 }
